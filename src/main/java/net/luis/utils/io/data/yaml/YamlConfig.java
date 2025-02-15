aa4686684817bef337b72c3bf6c5fb7a657006ee
Luis <luis@staudt-fridingen.de>
2025-02-15 16:03:27 +0100

    added yaml writer

---
+++ src/main/java/net/luis/utils/io/data/yaml/YamlConfig.java
@@ -0,0 +1,43 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.data.yaml;
+
+import net.luis.utils.io.data.config.WriteOnly;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public record YamlConfig(
+	@WriteOnly boolean useNullLiteral,
+	@WriteOnly boolean multilineStrings,
+	@WriteOnly boolean quoteSingleLineStrings,
+	@WriteOnly char quoteCharacter,
+	@WriteOnly @NotNull String indent,
+	@NotNull Charset charset
+) {
+	
+	public static final YamlConfig DEFAULT = new YamlConfig(true, true, false, '\'', "  ", StandardCharsets.UTF_8);
+}
---
+++ src/main/java/net/luis/utils/io/data/yaml/YamlWriter.java
@@ -0,0 +1,70 @@
+/*
+ * LUtils
+ * Copyright (C) 2025 Luis Staudt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.luis.utils.io.data.yaml;
+
+import net.luis.utils.io.data.OutputProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.Objects;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class YamlWriter implements AutoCloseable {
+	
+	private final YamlConfig config;
+	private final BufferedWriter writer;
+	
+	public YamlWriter(@NotNull OutputProvider output) {
+		this(output, YamlConfig.DEFAULT);
+	}
+	
+	public YamlWriter(@NotNull OutputProvider output, @NotNull YamlConfig config) {
+		this.config = Objects.requireNonNull(config, "Yaml config must not be null");
+		this.writer = new BufferedWriter(new OutputStreamWriter(Objects.requireNonNull(output, "Output must not be null").getStream(), config.charset()));
+	}
+	
+	public void writeYaml(@NotNull YamlStruct struct) {
+		Objects.requireNonNull(struct, "yaml struct must not be null");
+		try {
+			this.writer.write(struct.toString(this.config));
+			this.writer.newLine();
+			this.writer.newLine();
+			this.writer.flush();
+		} catch (IOException e) {
+			throw new UncheckedIOException("An I/O error occurred while writing the yaml struct", e);
+		}
+	}
+	
+	public void writeYaml(@NotNull YamlDocument document) {
+		Objects.requireNonNull(document, "Yaml document must not be null");
+		for (String key : document.keySet()) {
+			this.writeYaml(Objects.requireNonNull(document.getStruct(key), "yaml struct must not be null"));
+		}
+	}
+	
+	@Override
+	public void close() throws IOException {
+		this.writer.close();
+	}
+}
